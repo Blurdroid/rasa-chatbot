978ab8cf31ca7ebfa5acb6437f6292f02d9dbad4
ziademad <ziademadd20111@gmail.com>
2023-09-03 08:44:51 +0300

    make multiple fetch in database

--- actions/actions.py
+++ actions/actions.py
@@ -97,29 +97,82 @@ class GetFromSQLite_min(Action):
 
 class GetFromSQLite_getmarks(Action):
     def name(self) -> Text:
-        return "action_get_getmarks"
+        return "action_get_name_marks"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            # Read the required information from the user's input
-            entity_value = tracker.latest_message.get("entities")[0].get("value")
-
+        try:
             # Connect to the SQLite database
             conn = sqlite3.connect('resourceDB.db')
             cursor = conn.cursor()
 
             # Execute an SQL query to retrieve the data
-            query = "SELECT total_marks FROM students WHERE name=?;"
-            cursor.execute(query, (entity_value))
+            slot_value = tracker.get_slot('name')
+            user_input = tracker.latest_message
+            cursor.execute("SELECT name FROM students")
+            rows = cursor.fetchall()
+            query_name_list = [list(row) for row in rows] # convert sqlite3.cursor into list
+            dispatcher.utter_message(slot_value) # to see what inside slot_value
 
-            # Fetch all the results
-            results = cursor.fetchall()
+            if [slot_value] not in query_name_list:
+                dispatcher.utter_message("I can't find the student in the database!")
+            else:
+                # query = f"SELECT total_marks FROM students WHERE name={slot_value};" # to prevent SQL injection
+                query = "SELECT total_marks FROM students WHERE name=?;"
+                cursor.execute(query, (slot_value,))
+                results = cursor.fetchall()
+                if results:
+                    dispatcher.utter_message(str(results))
+                else:
+                    dispatcher.utter_message("empty!")
+        except Error as e:
+            dispatcher.utter_message(f"An error occurred while retrieving total marks from the database.")    
 
-            # Process the results as per your requirement
-            response = "retreiving from database:.."
-            for row in results:
-                response += row[0] + ", "
 
-            # Send the response to the user
-            dispatcher.utter_message(response)
+# class GetFromSQLite_getmarks(Action):
+#     def name(self) -> Text:
+#         return "action_get_name_marks"
+    
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+#             # Retrieve the specific entity from the latest message
+#             entity_value = None
+#             latest_message = tracker.latest_message
 
-            return []
+#             if 'entities' in latest_message:
+#                 entities = latest_message['entities']
+#                 for entity in entities:
+#                     if entity['entity'] == 'your_entity_name':
+#                         entity_value = entity['value']
+#                         break  # Exit loop if entity is found
+
+#             # Read the required information from the user's input
+#             # entity_value = tracker.latest_message.get("entities")[0].get("value")
+
+#             # Connect to the SQLite database
+#             conn = sqlite3.connect('resourceDB.db')
+#             cursor = conn.cursor()
+
+#             # Execute an SQL query to retrieve the data
+#             query = f"SELECT total_marks FROM students WHERE name='{entity_value}';"
+#             # bindings= (entity_value,) 
+#             cursor.execute(query)
+
+#             # Fetch all the results
+#             results = cursor.fetchall()
+
+#             if results is not None:
+#                 column_name, column_value = results
+#                 response = "The total marks of " + entity_value + " is " + int(column_value) #INT -> because the column name is total_marks ex:100
+#             else:
+#                 response = "No data found for "
+
+#             # Process the results as per your requirement
+#             # response = "retreiving from database:.."
+#             # for row in results:
+#             #     response += row[0] + ", "
+
+#             # Send the response to the user
+
+#             dispatcher.utter_message(results)
+
+#             return []
